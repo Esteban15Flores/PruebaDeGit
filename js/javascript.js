8eaad57bce82274ad85c509b404347fd7a4c1754
Esteban <estebanfr20100@gmail.com>
2021-05-06 00:14:28 -0500

    Estilos y jquery

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+function prueba() {
+  document.getElementById("demo").innerHTML = "Hello World";
+  $("#p").html("AAAAAAAAAAAAAAA");
+}
+
+function prueba2(){
+
+	$("#b").removeClass('bg-dark').addClass('bg-primary');
+
+
+}	
